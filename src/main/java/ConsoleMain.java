8f8a11344754fb97acfaab72725d6dff750d2b2e
junwoochoi <cjw4690@gmail.com>
2020-02-02 16:05:09 +0900

    refactor : Change Main to ConsoleMain

--- src/main/java/ConsoleMain.java
+++ src/main/java/ConsoleMain.java
@@ -1,4 +1,4 @@
-public class Main {
+public class ConsoleMain {
     public static void main(String[] args) {
         final GameController gameController = new GameController();
         gameController.init();
