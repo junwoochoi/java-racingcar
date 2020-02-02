810a19a4217da60f5cc9bbe50835b0288b9b2575
junwoochoi <cjw4690@gmail.com>
2020-02-02 21:44:37 +0900

    feat : listen 8080 port

--- src/main/java/WebMain.java
+++ src/main/java/WebMain.java
@@ -2,16 +2,17 @@ import view.WebRacingHandler;
 
 import java.util.HashMap;
 
-import static spark.Spark.get;
-import static spark.Spark.post;
+import static spark.Spark.*;
 import static util.RenderUtil.render;
 
 public class WebMain {
     private static WebRacingHandler webRacingHandler = new WebRacingHandler();
 
     public static void main(String[] args) {
+        port(8080);
 
         get("/", (req, res) -> render(new HashMap<>(), "/index.html"));
+
         post("/name", webRacingHandler.handleGetName());
 
         get("/result", webRacingHandler.handleResult());
